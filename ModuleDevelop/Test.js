cc237c9b54a8d5cf61e7d09c3a659034941f451e
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-06-03 20:49:13 +0900

    a

--- ModuleDevelop/Test.js
+++ ModuleDevelop/Test.js
@@ -1,36 +1,32 @@
 const http = require('http');
 
-const db = require('./DB/DB_Module');
-
 const dhm = require('./DHModule');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-// 서버 생성 시 
+// 클라이언트 접근 시
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
   db.status();
 
-  dhm.init();
   
 });
- 
+
+// 서버 생성 시
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-  
-  //db.setLoginInfo(, );
-  
-  var db_data = db.connect();
+  dhm.init();
 
-  
-  db.selectAllUserTable(db_data);
+  //db.room.room();
+  user = dhm.login('asd','123');
+  console.log(user);
+  console.log(user.code);
+  room = dhm.loadRoom(user.code);
+  console.log(room);
 
-  db.disconnect(db_data);
-
-  db.room.room();
 });
 
 
