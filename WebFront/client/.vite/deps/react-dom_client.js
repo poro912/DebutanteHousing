a2b354c47c0310f16235919993aae86cffc6f1f1
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-07-06 19:14:24 +0900

    Delete WebFront/client directory

--- WebFront/client/.vite/deps/react-dom_client.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_client
-} from "./chunk-WDFTZEMQ.js";
-import "./chunk-FLAVOKRJ.js";
-import "./chunk-AC2VUBZ6.js";
-export default require_client();
-//# sourceMappingURL=react-dom_client.js.map
