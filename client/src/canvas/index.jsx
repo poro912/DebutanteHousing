19b60ad1b284b3bf553a513d213e79791a0a86c8
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-07-10 12:35:25 +0900

    glb

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -11,6 +11,8 @@ import CameraRig from './CameraRig'
 import Room from './Room'
 import Candle from './Candle'
 import Glb from './Glb'
+import Glb2 from './Glb2'
+import RoomGlb from './RoomGlb'
 
 const CanvasModel = () => {
   const snap = useSnapshot(state);
@@ -18,7 +20,7 @@ const CanvasModel = () => {
   return (
     <Canvas
       shadows
-      camera={{ position: [160,90,160], fov:18 }}
+      camera={{ position: [16,9,16], fov:2 }}
       
       className=''
     >
@@ -26,21 +28,25 @@ const CanvasModel = () => {
       
       <ambientLight intensity={1} />
       <pointLight position={[0, 10, 10]} intensity={1.4} />
-      <Environment preset='city' />
+      <Environment preset='warehouse' />
 
       {/* <CameraRig> */}
         <Backdrop />
-          {state.intro === true ? <OrbitControls /> : <gridHelper args={[40, 10] } position={[0,-20,0]}/>}
+          {state.intro === true ? <OrbitControls /> : <group scale={[0.04, 0.04, 0.04]}><gridHelper args={[10, 10]} position={[0, -5, 0]} /></group>}
           
-          <Room pos={[0,0,0]} />
-          <Glb />
-          {
+
+          <OrbitControls />
+          {/* <Room pos={[0,0,0]} /> */}
+          <RoomGlb />
+          {/* <Glb /> */}
+          <Glb2 />
+          {/* {
             url.map((url, index) => {
               
               return <Candle idx={index} url={url[0]} mtl={url[1]} pos={url[2]} rotation={url[3]}/>
               
             })
-          }
+          } */}
           
         
       {/* </CameraRig> */}
