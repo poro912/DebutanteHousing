97f38c7faf7bcf1e4124d48d9056ab01e628272b
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-08-19 00:14:15 +0900

    metadataurl

--- Front/client/src/canvas/index.jsx
+++ Front/client/src/canvas/index.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import {Canvas} from '@react-three/fiber'
 import {Environment, Center, OrbitControls} from '@react-three/drei'
 import { useGLTF } from '@react-three/drei'
@@ -15,7 +16,21 @@ import Test from './Test'
 const CanvasModel = () => {
   const snap = useSnapshot(state);
   const url = state.morden
-  
+  const [glburl, setGlburl] = useState("")
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://gateway.pinata.cloud/ipfs/QmP25FavCnPjQpuvM9noxxfZNNRKW6cmUYsEg3LwSJ22gm/1.json');
+        const jsonData = await response.json();
+        console.log(jsonData.animation_url); // JSON 데이터를 콘솔에 출력
+        setGlburl(jsonData.animation_url);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <Canvas
       shadows
@@ -36,14 +51,14 @@ const CanvasModel = () => {
         <OrbitControls />
          
           <RoomGlb />
-          {/* <Glb /> */}
-          {
+          <Glb url={glburl} pos={[0,-0.2,0] }/>
+          {/* {
             url.map((url, index) => {
               console.log(url[0])
               return <Glb key={index} url={url[0]} pos={url[1]}/>
               
             })
-          } 
+          }  */}
           {/* <Test /> */}
         
       {/* </CameraRig> */}
