965b5d9eb60523d906a0dfcd17161f490a58868e
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-08-19 00:12:06 +0900

    blockchain backend

--- Front/server/controllers/contract.controller.js
+++ Front/server/controllers/contract.controller.js
@@ -7,11 +7,11 @@ const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'))
 //@notion 721 컨트랙트 설정
 const DHT721abi = require("../../contract/artifacts/contracts/DH721.sol/DH721.json")
 const abi721 = DHT721abi.abi;
-const DHT721 = new web3.eth.Contract(abi721, "0xBc258Df71A6d3a1e5CBb2B10290a9584932517AE");
+const DHT721 = new web3.eth.Contract(abi721, "0xA6021227bb9b2A7aff42C046F32d68be150080b5");
 
 const DHT20abi = require("../../contract/artifacts/contracts/DH20.sol/DH20.json")
 const abi20 = DHT20abi.abi;
-const DHT20 = new web3.eth.Contract(abi20, "0x1ABdd514Df2c6536f187c63B9d948912fae0130a");
+const DHT20 = new web3.eth.Contract(abi20, "0x006f96E335DB48C05f1f23D3137b02982b1cef71");
 
 
 module.exports = {
@@ -36,9 +36,9 @@ module.exports = {
     },
     setToken : async(req,res,next) => {
         try{
-            const {tokenAddress} = req.body;
+            const {account ,tokenAddress} = req.body;
             
-            const result = await DHT721.methods.setToken(tokenAddress).send({ from: '0x1440f3B95b1fFf757186287176C957C318093fDd' });
+            const result = await DHT721.methods.setToken(tokenAddress).send({ from: account });
             
             console.log(result);
             res.send(result);
@@ -47,19 +47,119 @@ module.exports = {
             res.status(500).send(e.message || e);
         }
     },
-    mintNFT : async(req, res, next) => {
+    mintNFT: async (req, res, next) => {
+        try {
+          const { recipient , tokenURI, price } = req.body;
+          const gasPrice = web3.utils.toWei('100', 'gwei'); // 원하는 가스 가격 설정
+          const gasLimit = 1000000; // 원하는 가스 리미트 설정
+      
+          const result = await DHT721.methods
+            .mintNFT(recipient, tokenURI, price)
+            .send({ from: recipient, gasPrice, gasLimit });
+      
+          console.log(result);
+          res.send(result);
+        } catch (e) {
+          console.error(e);
+          res.status(500).send(e.message || e);
+        }
+      },
+    saleNFT : async (req, res, next) => {
+        try {
+          const { account, tokenId, price } = req.body;
+      
+          const result = await DHT721.methods.saleNFT(tokenId, price).send({ from: account });
+      
+          console.log(result);
+          res.send(result);
+        } catch (e) {
+          console.error(e);
+          res.status(500).send(e.message || e);
+        }
+      },
+      buyNFT : async (req, res, next) => {
+        try {
+          const { account, tokenId } = req.body;
+          const gasPrice = web3.utils.toWei('100', 'gwei'); // 원하는 가스 가격 설정
+          const gasLimit = 1000000; // 원하는 가스 리미트 설정
+      
+          const result = await DHT721.methods.buyNFT( tokenId ).send({ from: account, gasPrice, gasLimit  });
+      
+          console.log(result);
+          res.send(result);
+        } catch (e) {
+          console.error(e);
+          res.status(500).send(e.message || e);
+        }
+      },
+      
+    getAllNftList : async(req, res, next) => {
         try{
-            const { recipient, tokenURI, price } = req.body;
-            const result = await DHT721.methods.mintNFT(recipient, tokenURI, price).send({ from: recipient });
-
-
+            const result = await DHT721.methods.getAllNftList().call()
+            
             console.log(result);
-            res.send(result);
+            res.status(200).json({
+                message : "getAllNftList",
+                data : {
+                    NFTList : result
+                }
+            });
         }catch(e){
             console.error(e);
             res.status(500).send(e.message || e);
         }
     },
+    getNftOwnerList : async(req, res, next) => {
+        try{
+            const { account } = req.body;
+            const result = await DHT721.methods.getNftOwnerList(account).call()
+            
+            console.log(result);
+            res.status(200).json({
+                message : "getNftOwnerList",
+                data : {
+                    NFTList : result
+                }
+            });
+        }catch(e){
+            console.error(e);
+            res.status(500).send(e.message || e);
+        }
+    },
+    getSaleAllNftList : async(req, res, next) => {
+        try{
+            const result = await DHT721.methods.getSaleAllNftList().call()
+            
+            console.log(result);
+            res.status(200).json({
+                message : "getNftOwnerList",
+                data : {
+                    NFTList : result
+                }
+            });
+        }catch(e){
+            console.error(e);
+            res.status(500).send(e.message || e);
+        }
+    },
+    getSaleOwnerNftList : async(req, res, next) => {
+        try{
+            const { account } = req.body;
+            const result = await DHT721.methods.getSaleOwnerNftList(account).call()
+            
+            console.log(result);
+            res.status(200).json({
+                message : "getNftOwnerList",
+                data : {
+                    NFTList : result
+                }
+            });
+        }catch(e){
+            console.error(e);
+            res.status(500).send(e.message || e);
+        }
+    },
+
     balanceOf : async(req, res, next) => {
         try{
             const { account } = req.body;
@@ -101,6 +201,24 @@ module.exports = {
             const { from, spender, amount } = req.body;
 
             const result = await DHT20.methods.approve(spender, amount).send({ from: from })
+            
+            console.log(result);
+            res.status(200).json({
+                message : "approve",
+                data : {
+                    bool : result
+                }
+            });
+        }catch(e){
+            console.error(e);
+            res.status(500).send(e.message || e);
+        }
+    },
+    allowance : async(req, res, next) => {
+        try{
+            const { from, spender } = req.body;
+
+            const result = await DHT20.methods.allowance(from, spender).call()
             
             console.log(result);
             res.status(200).json({
--- Front/server/routes/contract.route.js
+++ Front/server/routes/contract.route.js
@@ -7,8 +7,15 @@ router.get("/", controller.getAccounts);
 router.get("/new", controller.createAccount);
 router.post("/setToken", controller.setToken);
 router.post("/mintNFT", controller.mintNFT);
+router.post("/saleNFT", controller.saleNFT);
+router.post("/buyNFT", controller.buyNFT);
+router.post("/getAllNftList", controller.getAllNftList);
+router.post("/getNftOwnerList", controller.getNftOwnerList);
+router.post("/getSaleAllNftList", controller.getSaleAllNftList);
+router.post("/getSaleOwnerNftList", controller.getSaleOwnerNftList);
 router.post("/balanceOf", controller.balanceOf);
 router.post("/transfer", controller.transfer);
 router.post("/approve", controller.approve);
+router.post("/allowance", controller.allowance);
 
 module.exports = router;
