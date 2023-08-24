7df780a290b9f50a5bc497c90b9e2e84ea356524
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-08-24 20:35:08 +0900

    IPFS

--- Front/server/controllers/main.controller.js
+++ Front/server/controllers/main.controller.js
@@ -1,7 +1,51 @@
-exports.main_get = async(req, res, next) => {
-    try{
-        res.status(200).send("HI🍨")
-    } catch(e){
-        throw Error(e)
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
+const JWT = process.env.PINATA_JWT;
+module.exports = {
+  main_get: async (req, res, next) => {
+    try {
+      res.status(200).send("HI🍨");
+    } catch (e) {
+      throw Error(e);
     }
-}
+  },
+
+  uploadFileToIPFS: async (req, res, next) => {
+    try {
+      const uploadedFile = req.file;
+      const formData = new FormData();
+      console.log(formData)
+      formData.append('file', uploadedFile.buffer, {
+        filename: uploadedFile.filename, // Provide a filename
+      });
+
+      formData.append('pinataMetadata', JSON.stringify({
+        name: uploadedFile.originalname,
+      }));
+
+      formData.append('pinataOptions', JSON.stringify({
+        cidVersion: 0,
+      }));
+
+      const response = await axios.post(
+        'https://api.pinata.cloud/pinning/pinFileToIPFS',
+        formData,
+        {
+          headers: {
+            'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+            Authorization: JWT,
+          },
+        }
+      );
+
+      const ipfsHash = response.data.IpfsHash;
+      res.status(200).json({ ipfsHash });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  },
+};
--- Front/server/routes/main.route.js
+++ Front/server/routes/main.route.js
@@ -1,8 +1,15 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const multer = require('multer'); // Import multer for handling file uploads
 
-const controller = require("../controllers/main.controller")
+const controller = require('../controllers/main.controller');
 
-router.get("/", controller.main_get);
+// Initialize multer with desired options
+const upload = multer({ dest: 'uploads/' });
+
+router.get('/', controller.main_get);
+
+// Create a new route for handling file uploads
+router.post('/upload', upload.single('file'), controller.uploadFileToIPFS);
 
 module.exports = router;
