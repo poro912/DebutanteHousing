eca8a0c21350c788f8a2dd092f768f04b1853093
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-09-08 23:24:09 +0900

    conn nft

--- Server/routes/contract/controller.js
+++ Server/routes/contract/controller.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const Router = express();
+Router.use(express.json());
+
 const Web3 = require('web3');
 const Contract = require('web3-eth-contract');
 require('dotenv').config;
@@ -5,16 +9,16 @@ require('dotenv').config;
 //@notion web3 설정
 const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'))
 //@notion 721 컨트랙트 설정
-const DHT721abi = require("../../contract/artifacts/contracts/DH721.sol/DH721.json")
+const DHT721abi = require("../../../contract/artifacts/contracts/DH721.sol/DH721.json")
 const abi721 = DHT721abi.abi;
 const DHT721 = new web3.eth.Contract(abi721, "0xA6021227bb9b2A7aff42C046F32d68be150080b5");
 
-const DHT20abi = require("../../contract/artifacts/contracts/DH20.sol/DH20.json")
+const DHT20abi = require("../../../contract/artifacts/contracts/DH20.sol/DH20.json")
 const abi20 = DHT20abi.abi;
 const DHT20 = new web3.eth.Contract(abi20, "0x006f96E335DB48C05f1f23D3137b02982b1cef71");
 
 
-module.exports = {
+const Controller = {
     getAccounts : async(req, res, next) => {
         try {
             const accounts = await web3.eth.getAccounts();
@@ -214,7 +218,7 @@ module.exports = {
             res.status(500).send(e.message || e);
         }
     },
-    allowance : async(req, res, next) => {
+    allowance : async(req, res, next) => {Router
         try{
             const { from, spender } = req.body;
 
@@ -233,3 +237,20 @@ module.exports = {
         }
     },
 }
+
+Router.get("/", Controller.getAccounts);
+Router.get("/new", Controller.createAccount);
+Router.post("/setToken", Controller.setToken);
+Router.post("/mintNFT", Controller.mintNFT);
+Router.post("/saleNFT", Controller.saleNFT);
+Router.post("/buyNFT", Controller.buyNFT);
+Router.post("/getAllNftList", Controller.getAllNftList);
+Router.post("/getNftOwnerList", Controller.getNftOwnerList);
+Router.post("/getSaleAllNftList", Controller.getSaleAllNftList);
+Router.post("/getSaleOwnerNftList", Controller.getSaleOwnerNftList);
+Router.post("/balanceOf", Controller.balanceOf);
+Router.post("/transfer", Controller.transfer);
+Router.post("/approve", Controller.approve);
+Router.post("/allowance", Controller.allowance);
+
+module.exports = Router;
