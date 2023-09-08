0db47022057b065d3bef1a00693a675ff705eafb
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-09-08 23:31:21 +0900

    fixing items

--- Front/client/src/Redux/furnitureSlice.js
+++ Front/client/src/Redux/furnitureSlice.js
@@ -3,20 +3,20 @@ import { createSlice } from '@reduxjs/toolkit';
 const furnitureSlice = createSlice({
   name: 'furniture',
   initialState: {
-    furnitureList: [{ id: 0, url: '/glb/drawer/drawer_lightgreen.glb', pos: [-0.1,-0.2,-0], rot: [0, 0, 0] },
-                    { id: 0, url: '/glb/chair/chair1_green.glb', pos: [-0.1,-0.2,-0], rot: [0, 0, 0] },
-                    { id: 0, url: '/glb/closet1/closet1_lightgreen.glb', pos: [-0.1,-0.2,-0], rot: [0, 0, 0] },
-                    { id: 0, url: '/glb/bed/bed1_lightgreen_green.glb', pos: [-0.1,-0.2,-0], rot: [0, 0, 0] },
-                    { id: 0, url: '/glb/bed/bed1_lightgreen.glb', pos: [-0.1,-0.2,-0], rot: [0, 0, 0] }],
+    items: [{ id: 0, url: '/glb/drawer/drawer_lightgreen.glb', pos: [-0.1,-0.2,-0], rot: [0, 0, 0] },
+            { id: 0, url: '/glb/chair/chair1_green.glb', pos: [-0.1,-0.2,-0], rot: [0, 0, 0] },
+            { id: 0, url: '/glb/closet1/closet1_lightgreen.glb', pos: [-0.1,-0.2,-0], rot: [0, 0, 0] },
+            { id: 0, url: '/glb/bed/bed1_lightgreen_green.glb', pos: [-0.1,-0.2,-0], rot: [0, 0, 0] },
+            { id: 0, url: '/glb/bed/bed1_lightgreen.glb', pos: [-0.1,-0.2,-0], rot: [0, 0, 0] }],
   },
   reducers: {
     addFurniture: (state, action) => {
-      state.furnitureList.push(action.payload);
-      console.log('Updated furnitureList after adding:', state.furnitureList);
+      state.items.push(action.payload);
+      console.log('Updated items after adding:', state.items);
     },
     updateFurniture: (state, action) => {
       const { id, url, pos, rot } = action.payload;
-      const furnitureToUpdate = state.furnitureList.find(item => item.id === id);
+      const furnitureToUpdate = state.items.find(item => item.id === id);
       if (furnitureToUpdate) {
         furnitureToUpdate.url = url;
         furnitureToUpdate.pos = pos;
@@ -25,7 +25,7 @@ const furnitureSlice = createSlice({
     },
     removeFurniture: (state, action) => {
       const idToRemove = action.payload;
-      state.furnitureList = state.furnitureList.filter(item => item.id !== idToRemove);
+      state.items = state.items.filter(item => item.id !== idToRemove);
     },
     // 다른 액션들 추가 가능
   },
